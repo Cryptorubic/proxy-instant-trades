f170e8ee8261e93fa0f76b9c76bd870462df4f7d
debych <zh.eliseev@yandex.ru>
2022-11-08 13:27:13 +0300

    delete unused functions

--- contracts/InstantTradesProxy.sol
+++ contracts/InstantTradesProxy.sol
@@ -19,8 +19,6 @@ contract InstantProxy is BridgeBase {
     using EnumerableSetUpgradeable for EnumerableSetUpgradeable.AddressSet;
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
-    bool public swapsWithoutFeesEnabled;
-
     struct InstantTradesParams {
         address inputToken;
         uint256 inputAmount;
@@ -33,22 +31,9 @@ contract InstantProxy is BridgeBase {
 
     event DexSwap(address dex, address receiver, address inputToken, uint256 inputAmount, address outputToken);
 
-    modifier onlyWithoutFees() {
-        checkFees();
-        _;
-    }
-
     modifier onlyAvailableDex(address _dex) {
         checkDex(_dex);
         _;
-    }
-
-    /**
-     * @notice Used in modifier
-     * @dev Function to check if the fees are enabled
-     */
-    function checkFees() private view {
-        if (swapsWithoutFeesEnabled == false) revert FeesEnabled();
     }
 
     function checkDex(address _dex) private view {
@@ -77,30 +62,6 @@ contract InstantProxy is BridgeBase {
         __BridgeBaseInit(_fixedCryptoFee, _RubicPlatformFee, _routers, _tokens, _minTokenAmounts, _maxTokenAmounts);
     }
 
-    function instantTradeWithoutFees(
-        InstantTradesParams memory _params,
-        bytes calldata _data
-    ) external nonReentrant whenNotPaused onlyWithoutFees onlyAvailableDex(_params.dex) {
-        _params.inputAmount = _receiveTokens(_params.inputToken, _params.inputAmount);
-
-        _callDexWithChecksTokenInput(
-            _params,
-            _data
-        );
-    }
-
-    function instantTradeWithoutFeesNative(
-        InstantTradesParams memory _params,
-        bytes calldata _data
-    ) external payable nonReentrant whenNotPaused onlyWithoutFees onlyAvailableDex(_params.dex) {
-        _params.inputAmount = msg.value;
-
-        _callDexWithChecksNativeInput(
-            _params,
-            _data
-        );
-    }
-
     function instantTrade(
         InstantTradesParams memory _params,
         bytes calldata _data
@@ -117,6 +78,8 @@ contract InstantProxy is BridgeBase {
             _params,
             _data
         );
+
+        emit DexSwap(_params.dex, _params.recipient, _params.inputToken, _params.inputAmount, _params.outputToken);
     }
 
     function instantTradeNative(
@@ -139,6 +102,8 @@ contract InstantProxy is BridgeBase {
             _params,
             _data
         );
+
+        emit DexSwap(_params.dex, _params.recipient, _params.inputToken, _params.inputAmount, _params.outputToken);
     }
 
 
@@ -185,14 +150,6 @@ contract InstantProxy is BridgeBase {
         _amountIn = balanceAfterTransfer - balanceBeforeTransfer;
 
         return _amountIn;
-    }
-
-    function enableSwapsWithoutFee() external onlyManagerOrAdmin {
-        swapsWithoutFeesEnabled = true;
-    }
-
-    function disableSwapsWithoutFee() external onlyManagerOrAdmin {
-        swapsWithoutFeesEnabled = false;
     }
 
     function sweepTokens(address _token, uint256 _amount) external onlyAdmin {
